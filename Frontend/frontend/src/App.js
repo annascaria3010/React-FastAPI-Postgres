ee85742f04c68ab5bcec4e352206a7845627337e
annascaria3010 <annascaria01@gmail.com>
2025-01-22 17:59:37 +0900

    frontend folders

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar/navbar'
+import Sidebar from './Sidebar/sidebar'
 
+const App = () => {
+  return (
+    <div>
+    <Navbar/>
+    Welcome Home
+    <Sidebar/>
+    </div>
+  )
+}
+
+export default App
