7418a4824d822196633a3f772d2822e7673d7e20
annascaria3010 <annascaria01@gmail.com>
2025-01-23 11:59:37 +0900

    UI for navbar and home page

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/navbar'
 import Sidebar from './Sidebar/sidebar'
+import './App.css';
 
 const App = () => {
   return (
     <div>
-    <Navbar/>
-    Welcome Home
-    <Sidebar/>
+      <Navbar/>
+      Welcome Home
+      <Sidebar/>
     </div>
   )
 }
--- Frontend/frontend/src/Navbar/navbar.js
+++ Frontend/frontend/src/Navbar/navbar.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import './navbar.css'
+import { GiMushroomGills } from "react-icons/gi";
+import { FaHome } from "react-icons/fa";
+import { FiPhone } from "react-icons/fi";
+import { FaSearch } from 'react-icons/fa'; // FaSearch is the search icon from Font Awesome
+import { FiMenu } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='nav-main'>
+      <h3 className='small-title'>
+      <GiMushroomGills />Mush.
+      </h3>  
+      <div className='pages'>
+        <FaHome />Home 
+        
+        <FiPhone />Contact
+      </div>
+      <div className='icons'>
+        <FaSearch />
+        <FiUser />
+        <FiMenu />
+      </div>
+    </div>
   )
 }
 
